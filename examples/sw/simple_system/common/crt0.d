512554800f626b34baffc8517c9c5fbbffdc546d
Maicol Ciani <mciani@lagrev5.ee.ethz.ch>
2022-03-02 16:18:58 +0100

    cleaned useless stuff

--- examples/sw/simple_system/common/crt0.d
+++
@@ -1,3 +0,0 @@
-/scratch/ciani/opentitan/hw/top_titangrey/examples/sw/simple_system/common/crt0.o: \
- /scratch/ciani/opentitan/hw/top_titangrey/examples/sw/simple_system/common/crt0.S \
- /scratch/ciani/opentitan/hw/top_titangrey/examples/sw/simple_system/common/simple_system_regs.h
--- examples/sw/simple_system/common/simple_system_common.d
+++
@@ -1,4 +0,0 @@
-/scratch/ciani/opentitan/hw/top_titangrey/examples/sw/simple_system/common/simple_system_common.o: \
- /scratch/ciani/opentitan/hw/top_titangrey/examples/sw/simple_system/common/simple_system_common.c \
- /scratch/ciani/opentitan/hw/top_titangrey/examples/sw/simple_system/common/simple_system_common.h \
- /scratch/ciani/opentitan/hw/top_titangrey/examples/sw/simple_system/common/simple_system_regs.h
