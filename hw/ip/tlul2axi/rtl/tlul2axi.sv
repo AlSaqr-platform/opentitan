b57b0dc08e506ac39e495818f85d095d77f19808
Maicol Ciani <mciani@lagrev5.ee.ethz.ch>
2022-03-14 17:56:16 +0100

    modified tlul2axi module

--- hw/ip/tlul2axi/rtl/tlul2axi.sv
+++ hw/ip/tlul2axi/rtl/tlul2axi.sv
@@ -27,14 +27,13 @@ module tlul2axi #(
 
    );
    
-   enum logic [3:0] { IDLE, WAIT_B_VALID, WAIT_R_VALID } state_q, state_d;
-   logic dummy;
-   
+   enum logic [3:0] { IDLE, WAIT_B_VALID, WAIT_R_VALID, RESET } state_q, state_d;
+     
    always_comb begin : axi_fsm
        
     // Default assignments
         
- //   axi_mst.aw_valid  = 1'b0;
+    axi_mst.aw_valid  = 1'b0;
     axi_mst.aw_addr   = tl_host.tl_req.a_address;
     axi_mst.aw_prot   = 3'b0;
     axi_mst.aw_region = 4'b0;
@@ -48,7 +47,7 @@ module tlul2axi #(
     axi_mst.aw_atop   = '0;
     axi_mst.aw_user   = '0;
 
- //   axi_mst.ar_valid  = 1'b0;
+    axi_mst.ar_valid  = 1'b0;
     axi_mst.ar_addr   = tl_host.tl_req.a_address;
     axi_mst.ar_prot   = 3'b0;
     axi_mst.ar_region = 4'b0;
@@ -61,14 +60,14 @@ module tlul2axi #(
     axi_mst.ar_id     = tl_host.tl_req.a_source;
     axi_mst.ar_user   = '0;
 
- //   axi_mst.w_valid   = 1'b0;
+    axi_mst.w_valid   = 1'b0;
     axi_mst.w_data    = tl_host.tl_req.a_data;
     axi_mst.w_strb    = tl_host.tl_req.a_mask;
- //   axi_mst.w_last    = 1'b0;
+    axi_mst.w_last    = 1'b0;
     axi_mst.w_user    = '0;
 
- //   axi_mst.b_ready   = 1'b0;
- //   axi_mst.r_ready   = 1'b0;
+    axi_mst.b_ready   = 1'b0;
+    axi_mst.r_ready   = 1'b0;
 
     tl_host.tl_rsp.d_valid     = 1'b0;
     tl_host.tl_rsp.d_opcode    = tlul_pkg::AccessAck;
@@ -83,6 +82,16 @@ module tlul2axi #(
 
     case (state_q)
 
+      RESET: begin
+        axi_mst.b_ready   = 1'b0;
+        axi_mst.r_ready   = 1'b0;
+        axi_mst.w_valid   = 1'b0;
+        axi_mst.aw_valid  = 1'b0;
+        axi_mst.w_last    = 1'b0;
+        axi_mst.ar_valid  = 1'b0;
+        state_d = IDLE; 
+      end
+
       IDLE: begin
         axi_mst.b_ready   = 1'b0;
         axi_mst.r_ready   = 1'b0;
@@ -92,49 +101,50 @@ module tlul2axi #(
             axi_mst.ar_valid = 1'b1; 
             if(axi_mst.ar_ready) 
               state_d = WAIT_R_VALID;
-          end else begin                                     // put
+          end else if (tl_host.tl_req.a_opcode == tlul_pkg::PutFullData || tl_host.tl_req.a_opcode == tlul_pkg::PutPartialData) begin                                     
             axi_mst.w_last   = 1'b1;
             axi_mst.aw_valid = 1'b1;
             axi_mst.w_valid  = 1'b1;
             if(axi_mst.aw_ready && axi_mst.w_ready)
               state_d = WAIT_B_VALID;  
-          end 
-        end else                                // no request
-          state_d = IDLE;                  
-      end 
+          end                   
+        end
+      end
 
       WAIT_B_VALID: begin
 
-       axi_mst.b_ready = 1'b1;
-       tl_host.tl_rsp.d_valid  = 1'b1; 
-       tl_host.tl_rsp.d_source = axi_mst.b_id;
-       tl_host.tl_rsp.d_size   = axi_mst.aw_size[1:0];
-       tl_host.tl_rsp.d_opcode = tlul_pkg::AccessAck;
-       tl_host.tl_rsp.d_error  = axi_mst.b_resp[1];
-       axi_mst.aw_valid  = 1'b0;
-       axi_mst.w_valid   = 1'b0;
-       if(axi_mst.b_valid) begin
+        tl_host.tl_rsp.d_source = axi_mst.b_id;
+        tl_host.tl_rsp.d_size   = axi_mst.aw_size[1:0];
+        tl_host.tl_rsp.d_opcode = tlul_pkg::AccessAck;
+        tl_host.tl_rsp.d_error  = axi_mst.b_resp[1];
+        axi_mst.aw_valid  = 1'b0;
+        axi_mst.w_valid   = 1'b0;
+        axi_mst.w_last    = 1'b0;   
+        if(axi_mst.b_valid) begin
+          tl_host.tl_rsp.d_valid  = 1'b1;
+          axi_mst.b_ready = 1'b1;
           state_d = IDLE;
-       end else
+        end else
           state_d = WAIT_B_VALID;  
       end
 
       WAIT_R_VALID: begin
          
-        tl_host.tl_rsp.d_valid  = 1'b1;
+        
         tl_host.tl_rsp.d_source = axi_mst.r_id;
         tl_host.tl_rsp.d_size   = axi_mst.ar_size[1:0];
         tl_host.tl_rsp.d_opcode = tlul_pkg::AccessAckData;
         tl_host.tl_rsp.d_error  = axi_mst.r_resp[1];
         axi_mst.ar_valid  = 1'b0;
-        axi_mst.r_ready = 1'b1;
-        if(axi_mst.r_valid)
+        if(axi_mst.r_valid) begin
           state_d = IDLE;
-        else
+          axi_mst.r_ready = 1'b1;
+          tl_host.tl_rsp.d_valid  = 1'b1;
+        end else
           state_d = WAIT_R_VALID;
       end
 
-      default: state_d = IDLE;
+      default: state_d = RESET;
           
     endcase // case (state_q)
     
@@ -146,7 +156,7 @@ module tlul2axi #(
    
   always_ff @(posedge clk_i or negedge rst_ni) begin
     if (~rst_ni) 
-       state_q  <= IDLE;
+       state_q  <= RESET;
     else 
        state_q  <= state_d;
   end
