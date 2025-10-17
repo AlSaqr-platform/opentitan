57b6cb8ae35be0ff518b09a06db230b2205ab34c
Gabriele Lodi <gabriele.lodi93@gmail.com>
2025-10-17 16:43:50 +0200

    Fix simulation flow

--- opentitan.mk
+++ opentitan.mk
@@ -11,14 +11,15 @@
 #
 
 GIT ?= git
-BENDER ?= ./bender
+BENDER ?= bender
 VSIM ?= vsim
 DPI-LIB ?= work-dpi
 run_script := scripts/opentitan_start.tcl
 SRAM ?= ""
 BOOTMODE ?= 0
-QUESTA = questa-2022.3-bt
+QUESTA =
 IDMA_ROOT ?= $(shell $(BENDER) path idma)
+QUESTASIM_HOME ?= /tools/siemens/questa_2022.3/questasim
 
 cl-bin         ?= none
 OT_CLUSTER     = $(cl-bin)
@@ -50,7 +51,7 @@ ifdef nogui
 	GUI := -c
 endif
 
-VLOG_ARGS += -incr -64 -nologo -quiet -suppress vlog-2583 -suppress vlog-13314  +acc +nospecify +notimingchecks  -timescale \"1 ns / 1 ps\" 
+VLOG_ARGS += -incr -64 -nologo -quiet -suppress vlog-2583 -suppress vlog-13314 \"+incdir+\$$ROOT/hw/include\" +acc +nospecify +notimingchecks -timescale \"1 ns / 1 ps\"
 XVLOG_ARGS += -64bit -compile -vtimescale 1ns/1ns -quiet +nospecify +notimingchecks
 
 define generate_vsim
@@ -62,10 +63,14 @@ endef
 .PHONY: init build sim update clean secure_boot_jtag secure_boot_spi
 
 build:  $(dpi-library)/elfloader.so scripts/compile_opentitan.tcl scripts/compile_opentitan_vip.tcl $(OT_ROOT)/hw/tb/vips
-	$(QUESTA) vsim -c -do 'source $(compile_script); quit'
+	$(QUESTA) vsim -64 -c -do 'source $(compile_script); quit'
 
 sim: build
+<<<<<<< HEAD
 	$(QUESTA) vsim $(GUI) -do 'set SRAM $(SRAM); set OT_CLUSTER $(OT_CLUSTER); set BOOTMODE $(BOOTMODE); source $(run_script)'
+=======
+	$(QUESTA) vsim -64 -do 'set SRAM $(SRAM); set BOOTMODE $(BOOTMODE); source $(run_script)'
+>>>>>>> e34badec46 (Fix simulation flow)
 
 update:
 	$(BENDER) update
@@ -81,7 +86,8 @@ clean:
 	rm -rf uart
 
 scripts/compile_opentitan.tcl: Bender.yml
-	$(call generate_vsim, $@, -t use_idma -t rtl -t test -t snitch_cluster ,..)
+	$(BENDER) script $(VSIM) --vlog-arg="$(VLOG_ARGS)" -t use_idma -t rtl -t test -t snitch_cluster > $@
+# 	$(call generate_vsim, $@, -t use_idma -t rtl -t test -t snitch_cluster ,..)
 
 scripts/compile_opentitan_vip.tcl: Bender.yml
 	$(call generate_vsim, $@, -t use_idma -t rtl -t test_ot_vip -t snitch_cluster, ..)
@@ -106,14 +112,16 @@ $(OT_ROOT)/hw/tb/vips/s25fs256s.v:
 $(OT_ROOT)/hw/tb/vips:
 	rm -rf $@
 	mkdir $@
-	rm -rf model_tmp && mkdir model_tmp
-	cd model_tmp; wget https://www.infineon.com/dgdl/Infineon-S25fs256s-SimulationModels-v02_00-EN.zip?fileId=8ac78c8c7d0d8da4017d0f6251a24e7b
-	cd model_tmp; mv 'Infineon-S25fs256s-SimulationModels-v02_00-EN.zip?fileId=8ac78c8c7d0d8da4017d0f6251a24e7b' model.zip
-	cd model_tmp; unzip model.zip
-	cd model_tmp; mv 'S25fs256s' exe_folder
-	cd model_tmp/exe_folder; unzip S25fs256s.exe
-	cp model_tmp/exe_folder/S25fs256s/model/s25fs256s.v model_tmp/exe_folder/S25fs256s/model/s25fs256s_verilog.sdf $@
-	rm -rf model_tmp
+	wget --no-check-certificate --content-disposition "https://freemodelfoundry.com/fmf_vlog_models/flash/s25fs256s.v" -O ./hw/tb/vips/s25fs256s.v
+
+# 	rm -rf model_tmp && mkdir model_tmp
+# 	cd model_tmp; wget https://www.infineon.com/dgdl/Infineon-S25fs256s-SimulationModels-v02_00-EN.zip?fileId=8ac78c8c7d0d8da4017d0f6251a24e7b
+# 	cd model_tmp; mv 'Infineon-S25fs256s-SimulationModels-v02_00-EN.zip?fileId=8ac78c8c7d0d8da4017d0f6251a24e7b' model.zip
+# 	cd model_tmp; unzip model.zip
+# 	cd model_tmp; mv 'S25fs256s' exe_folder
+# 	cd model_tmp/exe_folder; unzip S25fs256s.exe
+# 	cp model_tmp/exe_folder/S25fs256s/model/s25fs256s.v model_tmp/exe_folder/S25fs256s/model/s25fs256s_verilog.sdf $@
+# 	rm -rf model_tmp
 
 init: bender update scripts/compile_opentitan.tcl scripts/compile_opentitan_vip.tcl $(OT_ROOT)/hw/tb/vips/s25fs256s.v
 
