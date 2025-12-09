25423fa3d03b7fe5dbe8c0a0bd1b84d4e4095df2
Gabriele Lodi <gabriele.lodi93@gmail.com>
2025-12-09 15:58:32 +0100

    Improve sim commands, add missing targs, some cleanup

--- opentitan.mk
+++ opentitan.mk
@@ -44,18 +44,14 @@ top_level ?= testbench_asynch_astral
 
 vsim_args += +notimingchecks +nospecify
 
+compile_script := scripts/compile_opentitan.tcl
+
 ifdef flash_preload
   VLOG_ARGS += +define+FLASH_PRELOAD
 endif
 
 ifdef jtag_sec_boot
   VLOG_ARGS += +define+JTAG_SEC_BOOT
-endif
-
-ifdef vip
-compile_script := scripts/compile_opentitan_vip.tcl
-else
-compile_script := scripts/compile_opentitan.tcl
 endif
 
 ifdef nogui
@@ -84,7 +80,7 @@ endef
 generate_idma_rtl:
 	$(MAKE) -C $(shell find $(BENDER_GIT_DIR) -type d -name 'idma*' | head -n 1) idma_hw_all
 
-build:  $(dpi-library)/elfloader.so scripts/compile_opentitan.tcl scripts/compile_opentitan_vip.tcl $(OT_ROOT)/hw/tb/vips
+build:  $(dpi-library)/elfloader.so scripts/compile_opentitan.tcl $(OT_ROOT)/hw/tb/vips
 	$(QUESTA) qsim -c -do 'source $(compile_script); quit'
 
 build_tech_mem: build
@@ -98,27 +94,24 @@ build_tech_mem: build
 		vlog -incr +define+INITIALIZE_MEM -work $(library) $(VER_DIR)/$(mem).v;\
 	)
 
-sim_no_gui: generate_idma_rtl build
-	qopt -work $(library) ${top_level} -o ${top_level}_opt
-	qsim -c ${top_level}_opt -t 1ps -suppress 3999 -suppress 8360 \
-	-do "$(do_command)" \
+sim_rtl: generate_idma_rtl
+	qopt $(vopt_args) -work $(library) ${top_level} -o ${top_level}_opt
+	qsim $(vsim_args) ${top_level}_opt -t 1ps -suppress 3999 -suppress 8360 \
+	-do "$(do_command)"	\
 	+SRAM=${SRAM} +OT_CLUSTER=${OT_CLUSTER} +BOOTMODE=${BOOTMODE} -sv_lib $(dpi-library)/elfloader
 
-sim_rtl: generate_idma_rtl build
-	qopt -debug +designfile -work $(library) ${top_level} -o ${top_level}_opt
-	qsim -qwavedb=+signal+memory ${top_level}_opt -t 1ps -suppress 3999 -suppress 8360 \
-	-do "set StdArithNoWarnings 1; set NumericStdNoWarnings 1;"	\
-	+SRAM=${SRAM} +OT_CLUSTER=${OT_CLUSTER} +BOOTMODE=${BOOTMODE} -sv_lib $(dpi-library)/elfloader
-
-sim_rtl_tech_mem: generate_idma_rtl build_tech_mem
+sim_rtl_tech_mem: generate_idma_rtl
 	qopt  $(vopt_args) -work $(library) ${top_level} -o ${top_level}_opt
 	qsim  $(vsim_args) ${top_level}_opt -t 1ps -suppress 3999 -suppress 8360 \
 	$(vsim_args) +init_mem_data=0 \
 	-do "$(do_command)" \
 	+SRAM=${SRAM} +OT_CLUSTER=${OT_CLUSTER} +BOOTMODE=${BOOTMODE} -sv_lib $(dpi-library)/elfloader
 
-sim_gls:
-	$(MAKE) -C target/gf22/questasim int_gls_sim
+sim_gls_compile:
+	$(MAKE) -C target/gf22/questasim sim_gls_compile
+
+sim_gls_run:
+	$(MAKE) -C target/gf22/questasim sim_gls_run
 
 update:
 	$(BENDER) update
@@ -133,19 +126,26 @@ clean:
 	rm -rf vsim.wlf
 	rm -rf uart
 
-common_defs  += -D FEATURE_ICACHE_STAT
+
+# Targets #
 common_targs += -t cv32e40p_use_ff_regfile
+common_targs += -t use_idma
+common_targs += -t idma
+common_targs += -t rtl
+common_targs += -t snitch_cluster
+common_targs += -t test
+common_targs += -t test_ot_vip
+common_targs += -t tech_sim
+common_targs += -t simulation
+# Define #
+common_defs  += -D FEATURE_ICACHE_STAT
 common_defs  += -D PRIVATE_ICACHE
 common_defs  += -D HIERARCHY_ICACHE_32BIT
 common_defs  += -D ICACHE_USE_FF
 common_defs  += -D CLUSTER_ALIAS
 
 scripts/compile_opentitan.tcl: Bender.yml
-	$(BENDER) script $(VSIM) --vlog-arg="$(VLOG_ARGS)" -t use_idma -t rtl -t test -t snitch_cluster $(common_defs) $(common_targs) > $@
-# 	$(call generate_vsim, $@, -t use_idma -t rtl -t test -t snitch_cluster ,..)
-
-scripts/compile_opentitan_vip.tcl: Bender.yml
-	$(call generate_vsim, $@, -t use_idma -t rtl -t test_ot_vip -t snitch_cluster, ..)
+	$(BENDER) script $(VSIM) --vlog-arg="$(VLOG_ARGS)" $(common_defs) $(common_targs) > $@
 
 secure_boot_jtag:
 	make clean sim BOOTMODE=0 SRAM=sw/tests/opentitan/flash_preload_hmac_smoketest/flash_preload_hmac_smoketest.elf jtag_sec_boot=1
@@ -158,7 +158,7 @@ $(OT_ROOT)/hw/tb/vips:
 	mkdir $@
 	wget --no-check-certificate --content-disposition "https://freemodelfoundry.com/fmf_vlog_models/flash/s25fs256s.v" -O ./hw/tb/vips/s25fs256s.v
 
-init: update scripts/compile_opentitan.tcl scripts/compile_opentitan_vip.tcl $(OT_ROOT)/hw/tb/vips
+init: update scripts/compile_opentitan.tcl $(OT_ROOT)/hw/tb/vips
 
 # DPI
 dpi := $(patsubst hw/tb/dpi/%.cc, ${dpi-library}/%.o, $(wildcard hw/tb/dpi/*.cc))
