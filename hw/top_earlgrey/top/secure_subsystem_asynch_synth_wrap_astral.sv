e092c15b706d1387799cf4f7ce620e7d375e431d
Gabriele Lodi <gabriele.lodi93@gmail.com>
2026-01-13 11:23:15 +0100

    [L2] Some clean up and refactoring

--- hw/top_earlgrey/top/secure_subsystem_asynch_synth_wrap_astral.sv
+++ hw/top_earlgrey/top/secure_subsystem_asynch_synth_wrap_astral.sv
@@ -41,7 +41,7 @@ module security_island
    parameter type axi_ext_r_chan_t = synth_axi_ext_r_chan_t,
    parameter type axi_ext_req_t = synth_axi_ext_req_t,
    parameter type axi_ext_resp_t = synth_axi_ext_resp_t,
-   // Synchronizaton parameters
+   // Synchronization parameters
    parameter int unsigned LogDepth = SynthLogDepth,
    parameter int unsigned CdcSyncStages = SynthCdcSyncStages,
    parameter int unsigned SyncStages = 3,
@@ -489,6 +489,8 @@ module security_island
   /////////////////////
   // L2 memory slave //
   /////////////////////
+
+  // parameters
   localparam int unsigned L2MemSize = 512*1024;
   localparam int unsigned MemDataWidth = 32;
   // NumBanks = 2 * AxiDataWidth / MemDataWidth = 4 banks is the min value
@@ -496,6 +498,7 @@ module security_island
   localparam int unsigned NumBanks = 16;
   localparam int unsigned L2BankSize = L2MemSize / NumBanks;
 
+  //  signals
   logic [NumBanks-1:0]                         l2_mem_slave_req;
   logic [NumBanks-1:0]                         l2_mem_slave_gnt;
   logic [NumBanks-1:0]                         l2_mem_slave_we;
@@ -557,24 +560,19 @@ module security_island
     assign l2_mem_slave_gnt[i] = 1'b1;
 
     tc_sram #(
-      .NumWords   (L2BankSize  ), // Number of Words in data array
-      .DataWidth  (AxiDataWidth), // Data signal width
-      .NumPorts   (1           ), // Number of read and write ports
-    `ifndef SYNTHESIS
-      .ByteWidth  (8        ), // Width of a data byte
-      .SimInit    ("ones"   ), // Simulation initialization
-      .PrintSimCfg(0        ), // Print configuration
-    `endif
-      .Latency    (1        ) // Latency when the read data is available
+      .NumWords  ( L2BankSize   ), // Number of Words in data array
+      .DataWidth ( AxiDataWidth ), // Data signal width
+      .NumPorts  ( 1            ), // Number of read and write ports
+      .Latency   ( 1            )  // Latency when the read data is available
     ) i_bank (
-      .clk_i  (clk_i                 ), // Clock
-      .rst_ni (rst_ni                ), // Asynchronous reset active low
-      .req_i  (l2_mem_slave_req   [i]), // request
-      .we_i   (l2_mem_slave_we    [i]), // write enable
-      .addr_i ( {l2_mem_slave_add [i][$clog2(L2BankSize)-1:2], 2'b00} ), // request address
-      .wdata_i(l2_mem_slave_data  [i]), // write data
-      .be_i   (l2_mem_slave_be    [i]), // write byte enable
-      .rdata_o(l2_mem_slave_r_data[i])  // read data
+      .clk_i   ( clk_i                  ), // Clock
+      .rst_ni  ( rst_ni                 ), // Asynchronous reset active low
+      .req_i   ( l2_mem_slave_req   [i] ), // request
+      .we_i    ( l2_mem_slave_we    [i] ), // write enable
+      .addr_i  (  {l2_mem_slave_add [i][$clog2(L2BankSize)-1:2], 2'b00} ), // request address
+      .wdata_i ( l2_mem_slave_data  [i] ), // write data
+      .be_i    ( l2_mem_slave_be    [i] ), // write byte enable
+      .rdata_o ( l2_mem_slave_r_data[i] )  // read data
     );
 
   end
