03daa25d27e80fc9f4d6bf4c529b8b6eccf0573c
Gabriele Lodi <gabriele.lodi93@gmail.com>
2026-02-05 14:12:12 +0100

    [SW] Add L2 addressability to carfield_mmap_test

--- sw/tests/carfield/carfield_mmap_test/carfield_mmap_test.c
+++ sw/tests/carfield/carfield_mmap_test/carfield_mmap_test.c
@@ -14,11 +14,11 @@
 #include <stdbool.h>
 
 
-#define L2_BASE_ADDR 0x78000000
+#define L2_BASE_ADDR 0xA0000000
 #define L3_BASE_ADDR 0x80400000
 #define EOC 0xc11c0018
 #define NUM_ITER 30
-// #define L2
+#define L2
 #define L3
 
 int main(int argc, char **argv) {
