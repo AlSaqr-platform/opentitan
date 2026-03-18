b1d99340e56061e99d1790da75330cf34ed296ac
Pierangelo Rapa <pierangelo.rapa@gmail.com>
2026-03-18 06:17:06 -0400

    Added Snooper test, generated rv_plic_regs.h

--- sw/sw.mk
+++ sw/sw.mk
@@ -1,9 +1,12 @@
 PULP_SW_DIR  := $(PULP_REGR_DIR)
 
 GENERIC_TEST := $(TESTS_DIR)/cluster_offload
+SNOOPER_TEST := $(TESTS_DIR)/snooper_test
 
 $(GENERIC_TEST)/cluster_offload.elf:
 	$(MAKE) -C $(GENERIC_TEST) clean all
+$(SNOOPER_TEST)/snooper_test.elf:
+	$(MAKE) -C $(SNOOPER_TEST) clean all
 
 PULP_TEST_DIRS := $(filter-out %deeploy %neureka, $(wildcard $(PULP_SW_DIR)/opentitan-cluster/*))
 NEUREKA_TEST_DIRS := $(wildcard $(PULP_SW_DIR)/opentitan-cluster/neureka/*)
