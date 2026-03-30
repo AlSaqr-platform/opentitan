a3dc6e25954f7879598b8d6823bcc5ba6f7e4dde
Luigi Ghionda <luigi.ghio.98@gmail.com>
2026-03-31 01:21:19 +0200

    [Makefile] Add Deeploy tests to sw compilation flow and regression YAML

--- sw/sw.mk
+++ sw/sw.mk
@@ -5,11 +5,16 @@ GENERIC_TEST := $(TESTS_DIR)/cluster_offload
 $(GENERIC_TEST)/cluster_offload.elf:
 	$(MAKE) -C $(GENERIC_TEST) clean all
 
-PULP_TEST_DIRS := $(wildcard $(PULP_SW_DIR)/opentitan-cluster/*)
+PULP_TEST_DIRS := $(filter-out %deeploy, $(wildcard $(PULP_SW_DIR)/opentitan-cluster/*))
+DEEPLOY_TEST_DIRS := $(wildcard $(PULP_SW_DIR)/opentitan-cluster/deeploy/*/*)
 
 PULP_BUILD_TARGETS := $(addsuffix /build,$(PULP_TEST_DIRS))
+DEEPLOY_BUILD_TARGETS := $(addsuffix /build,$(DEEPLOY_TEST_DIRS))
+
+$(PULP_SW_DIR)/opentitan-cluster/deeploy/%/build:
+	$(MAKE) -C $(PULP_SW_DIR)/opentitan-cluster/deeploy/$* clean pulp_nn all
 
 $(PULP_SW_DIR)/%/build:
 	$(MAKE) -C $(PULP_SW_DIR)/$* clean all
 
-pulpd-sw-all: $(PULP_BUILD_TARGETS)
+pulpd-sw-all: $(PULP_BUILD_TARGETS) $(DEEPLOY_BUILD_TARGETS)
